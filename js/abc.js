3cdf82d3ab9cd3c1f256bb1a5fbf6e73eba69a0d
Jesse Sutherland <jessesutherland06@gmail.com>
2019-07-09 00:01:36 -0500

    Clean up

--- js/abc.js
+++ js/abc.js
@@ -1,25 +1,24 @@
-//Vue.use(VueHammer);
 
-// Vue.directive("pan", {
-//     bind: function (el, binding) {
-//         if (typeof binding.value === "function") {
-//             const mc = new Hammer(el);
-//             mc.get("pan").set({ direction: Hammer.DIRECTION_ALL });
-//             mc.on("pan", binding.value);
-//         }
-//     }
-// });
+Vue.directive("pan", {
+    bind: function(el, binding) {
+        if (typeof binding.value === "function") {
+            var mc = new Hammer(el);
+            mc.get("pan").set({ direction: Hammer.DIRECTION_ALL });
+            mc.on("pan", binding.value);
+        }
+    }
+});
 
-// Vue.directive("tap", {
-//     bind: function (el, binding) {
-//         if (typeof binding.value === "function") {
-//             const mc = new Hammer(el);
-//             mc.on("tap", binding.value);
-//         }
-//     }
-// });
+Vue.directive("tap", {
+    bind: function(el, binding) {
+        if (typeof binding.value === "function") {
+            var mc = new Hammer(el);
+            mc.on("tap", binding.value);
+        }
+    }
+});
+
 //https://lisilinhart.info/posts/touch-interaction-vue/
-//https://codepen.io/lisilinhart/pen/wxRQBo
 
 var app = new Vue({
     el: '#app',
@@ -31,6 +30,10 @@ var app = new Vue({
         currentArray: 'alphabetUpper',
         mode: 'sheet',
         singleModeIndex: 0,
+        colorPopup: false,
+        currentColor: 'purple',
+        colorsArray: ['purple','blue','orange','red','green'],
+        randomKey: '45675768',
     },
     mounted: function () {
         this.createArray();
@@ -45,30 +48,53 @@ var app = new Vue({
             tempShuffled.forEach(function (element) {
                 this.visibleArray.push({ character: element, visible: true })
             }, this);
+            this.randomKey = Math.random().toString(36).substr(2, 9);
         },
         randomize: function () {
             this.createArray();
         },
         changeArray: function(type) {
             this.currentArray = type;
-            this.visibleArray = [];
-            var tempShuffled = this.shuffle(this[this.currentArray]);
-            tempShuffled.forEach(function (element) {
-                this.visibleArray.push({character: element, visible: true})
-            }, this);
+            this.createArray();
         },
-        showNewSingleLetter: function() {
-            var arrayLength = this[this.currentArray].length - 1;
-            if (this.singleModeIndex < arrayLength) {
-                this.singleModeIndex++;
-            } else {
-                this.shuffle(this[this.currentArray]);
-                this.singleModeIndex = 0;
+        showNewSingleLetter: function(event) {
+            // var deltaX = event.deltaX; // moved distance on x-axis
+            // var deltaY = event.deltaY; // moved distance on y-axis
+            // var direction = event.direction; // 0 = none, 2 = left, 4 = right, 8 = up, 16 = down,
+            var isFinal = event.isFinal; // pan released
+            if (isFinal) {
+                var arrayLength = this[this.currentArray].length - 1;
+                if (this.singleModeIndex < arrayLength) {
+                    this.singleModeIndex++;
+                } else {
+                    this.shuffle(this[this.currentArray]);
+                    this.singleModeIndex = 0;
+                }
             }
-
         },
-        hideSingleLetter: function (character) {
-            character.visible = false;
+        hideSingleLetter: function (event, character) {
+            // var deltaX = event.deltaX; // moved distance on x-axis
+            // var deltaY = event.deltaY; // moved distance on y-axis
+            // var direction = event.direction; // 0 = none, 2 = left, 4 = right, 8 = up, 16 = down,
+            var isFinal = event.isFinal; // pan released
+            if (isFinal) {
+                if (character.visible === false) {
+                    character.visible = true;
+                } else {
+                    character.visible = false;
+                }
+            }
+        },
+        changeColor: function(event, color) {
+            this.currentColor = color;
+            this.colorPopup = false;
+        },
+        colorClass: function(color) {
+            if (color === this.currentColor) {
+                return color + ' active';
+            } else {
+                return color;
+            }
         },
         shuffle: function (array) {
             var currentIndex = array.length, temporaryValue, randomIndex;
